0620819155009a0bb6058589887f3a2e78aebdc0
jaw1306 <jaw130609@gmail.com>
2026-09-09 12:18:15 +0700

    chore: save local changes before rename branch

--- app/Http/Controllers/HallOfFameController.php
+++ app/Http/Controllers/HallOfFameController.php
@@ -8,7 +8,7 @@ class HallOfFameController extends Controller
 {
     public function index()
     {
-        $hallOfFameList = [
+        $HallOfFameList = [
             [
                 'id'          => 1,
                 'event'       => 'WorldSkills Shanghai 2026',
@@ -27,27 +27,27 @@ class HallOfFameController extends Controller
             ],
         ];
 
-        return view('hall-of-fame.index', [
+        return view('Hall-of-fame.index', [
             'title'          => 'Hall of Fame',
-            'hallOfFameList' => $hallOfFameList,
+            'HallOfFameList' => $HallOfFameList,
         ]);
     }
 
     public function create()
     {
-        return view('hall-of-fame.create', [
+        return view('Hall-of-fame.create', [
             'title' => 'Add New Achievement',
         ]);
     }
 
     public function store(Request $request)
     {
-        return redirect()->route('hall-of-fame.index');
+        return redirect()->route('Hall-of-fame.index');
     }
 
     public function edit($id = null)
     {
-        $hallOfFame = [
+        $HallOfFame = [
             'id'          => $id ?? 1,
             'event'       => 'International Physics Olympiad (IPhO) 2026',
             'field'       => 'Theoretical & Experimental Physics',
@@ -56,19 +56,19 @@ class HallOfFameController extends Controller
             'achievement' => '2nd Winner - Silver Medal',
         ];
 
-        return view('hall-of-fame.edit', [
+        return view('Hall-of-fame.edit', [
             'title'      => 'Edit Achievement',
-            'hallOfFame' => $hallOfFame,
+            'HallOfFame' => $HallOfFame,
         ]);
     }
 
     public function update(Request $request, $id)
     {
-        return redirect()->route('hall-of-fame.index');
+        return redirect()->route('Hall-of-fame.index');
     }
 
     public function destroy($id)
     {
-        return redirect()->route('hall-of-fame.index');
+        return redirect()->route('Hall-of-fame.index');
     }
 }
